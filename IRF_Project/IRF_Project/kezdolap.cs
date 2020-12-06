14817801c06b15d9e1445b42185df3115026d7be
Mate Janos <janos.mate@bcecid.net>
2020-12-06 19:34:21 +0100

    formok megnyitása

--- IRF_Project/IRF_Project/kezdolap.cs
+++ IRF_Project/IRF_Project/kezdolap.cs
@@ -28,22 +28,74 @@ namespace IRF_Project
                     {
                         db.BackColor = Color.Red;
                         db.Name = qw.ToString();
+                        db.Click += new EventHandler(this.button_click);
                         panel1.Controls.Add(db);
                     }
-                    if (qw % 6 == 0)
+                    else if (qw % 6 == 0)
                     {
                         db.BackColor = Color.Blue;
                         db.Name = qw.ToString();
+                        db.Click += new EventHandler(this.button_click);
                         panel1.Controls.Add(db);
                     }
                     else
                     {
                         db.Name = qw.ToString();
+                        db.Click += new EventHandler(this.button_click);
                         panel1.Controls.Add(db);
                     }
                     qw++;
                 }
 
+            }
+        }
+        void button_click(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+            if (btn.Name == "1")
+            {
+                Form1 f1 = new Form1();
+                f1.Show();
+            }
+            if (btn.Name == "2")
+            {
+                Form2 f2 = new Form2();
+                f2.Show();
+            }
+            if (btn.Name == "3")
+            {
+                Form3 f3 = new Form3();
+                f3.Show();
+            }
+            if (btn.Name == "4")
+            {
+                Form4 f4 = new Form4();
+                f4.Show();
+            }
+            if (btn.Name == "5")
+            {
+                Form5 f5 = new Form5();
+                f5.Show();
+            }
+            if (btn.Name == "6")
+            {
+                Form6 f6 = new Form6();
+                f6.Show();
+            }
+            if (btn.Name == "7")
+            {
+                Form7 f7 = new Form7();
+                f7.Show();
+            }
+            if (btn.Name == "8")
+            {
+                Form8 f8 = new Form8();
+                f8.Show();
+            }
+            if (btn.Name == "9")
+            {
+                Form9 f9 = new Form9();
+                f9.Show();
             }
         }
     }
