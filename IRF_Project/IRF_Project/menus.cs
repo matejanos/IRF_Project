cb4a944c1100d52457eadf8206e353949a15c4a8
Mate Janos <janos.mate@bcecid.net>
2020-12-07 16:09:17 +0100

    menus osztály létrehozása

---
+++ IRF_Project/IRF_Project/menus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IRF_Project
+{
+    public class menus
+    {
+        public string Leves { get; set; }
+        public string Foetel { get; set; }
+        public string Desszert { get; set; }
+        public int ID { get; set; }
+    }
+}
